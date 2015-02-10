81b5e593a15462a01066ebc5eb05a767d643f20f
Benedict Lee <benedictleejh@gmail.com>
2015-02-10 22:59:39 +0800

    Slightly expanded Main

--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/Main.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/Main.scala
@@ -1,7 +1,16 @@
 package org.narrativeandplay.hypedyn
 
 import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.layout.BorderPane
 
-object Main extends JFXApp{
+object Main extends JFXApp {
+  stage = new PrimaryStage {
+    title = "HypeDyn"
 
+    scene = new Scene {
+      root = new BorderPane()
+    }
+  }
 }
