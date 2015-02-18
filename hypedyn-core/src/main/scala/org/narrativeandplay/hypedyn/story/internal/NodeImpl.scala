71c21d63ed85f68a9175e9951dc50a52f670ec13
Benedict Lee <benedictleejh@gmail.com>
2015-02-18 17:06:08 +0800

    Replaced implementation of story model in core

---
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/internal/NodeImpl.scala
@@ -0,0 +1,5 @@
+package org.narrativeandplay.hypedyn.story.internal
+
+import org.narrativeandplay.hypedyn.story.Node
+
+class NodeImpl(var name: String, var content: String, val id: Long) extends Node
