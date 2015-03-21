60fa1ffc87c80785fb66fd410580c66b315ae66b
Benedict Lee <benedictleejh@gmail.com>
2015-03-21 16:31:28 +0800

    Deal with stupid Java NullPointerExceptions
    A File.listFiles returns null when the file does not exist, instead of
    something more sane.

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/plugins/PluginsController.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/plugins/PluginsController.scala
@@ -24,21 +24,22 @@ trait PluginsController {
   val Plugins = mutable.HashMap.empty[String, T]
 
   def init(): Unit = {
-    val pluginJars = PluginFolder.listFiles().filter(_.getName.endsWith(".jar"))
-    val classpath = List(new File(".")) ++ pluginJars
+    val pluginFolderFiles = Option(PluginFolder.listFiles())
+    val pluginJars = pluginFolderFiles.getOrElse(Array[File]()) filter (_.getName.endsWith(".jar"))
+    val classpath = ClassFinder.classpath ++ pluginJars
     val finder = ClassFinder(classpath)
     val classes = finder.getClasses()
     val classMap = ClassFinder.classInfoMap(classes.iterator)
     val pluginsToLoad = ClassFinder.concreteSubclasses(PluginClassName, classMap)
 
     val loader = new URLClassLoader(pluginJars.map({ f => new URL(s"file:${f.getAbsolutePath}") }), ClassLoader.getSystemClassLoader)
-    val mirror = runtimeMirror(getClass.getClassLoader)
 
     pluginsToLoad.map(_.name).foreach { pluginClassName =>
       val plugin = loader.loadClass(pluginClassName).newInstance().asInstanceOf[T]
 
       // Hack to get the plugin name
       // This is ok only because all plugin types must declare a self-type annotation on Plugin
+      // I.e. all plugins must also implement the Plugin trait
       Plugins += plugin.asInstanceOf[Plugin].name -> plugin
     }
   }
