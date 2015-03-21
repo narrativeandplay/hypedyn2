e6eea51c43342c0ff37ca258e0c998af1fc1d218
Benedict Lee <benedictleejh@gmail.com>
2015-03-21 16:32:26 +0800

    Set up main window

--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/Main.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/Main.scala
@@ -1,16 +1,24 @@
 package org.narrativeandplay.hypedyn
 
+import org.narrativeandplay.hypedyn.plugins.storyviewer.StoryViewersController
+
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.layout.BorderPane
+import scalafx.scene.layout.{VBox, BorderPane}
 
 object Main extends JFXApp {
   stage = new PrimaryStage {
     title = "HypeDyn"
 
     scene = new Scene {
-      root = new BorderPane()
+      root = new BorderPane() {
+        top = new VBox() {
+          children.addAll(MenuBar.menuBar, Toolbar.toolbar)
+        }
+
+        center = StoryViewersController.defaultViewer
+      }
     }
   }
 }
--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/plugins/storyviewer/StoryViewersController.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/plugins/storyviewer/StoryViewersController.scala
@@ -2,7 +2,11 @@ package org.narrativeandplay.hypedyn.plugins.storyviewer
 
 import org.narrativeandplay.hypedyn.plugins.PluginsController
 
+import scalafx.scene.control.Control
+
 object StoryViewersController extends PluginsController{
   type T = StoryViewerLike
   override val PluginClassName = classOf[StoryViewerLike].getCanonicalName
+  
+  val defaultViewer: Control = Plugins("Default Story Viewer").asInstanceOf[Control]
 }
