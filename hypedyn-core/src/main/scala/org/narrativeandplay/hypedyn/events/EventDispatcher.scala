abaf1200e474d6718c53a2cf35fb1e37ecfc11de
Benedict Lee <benedictleejh@gmail.com>
2015-04-05 16:12:17 +0800

    Refactor to take advantage of Scala operator style function calls

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/events/EventDispatcher.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/events/EventDispatcher.scala
@@ -3,7 +3,7 @@ package org.narrativeandplay.hypedyn.events
 import org.narrativeandplay.hypedyn.story.StoryController
 
 object EventDispatcher {
-  EventBus.createNodeEvents subscribe { evt => StoryController.addNode(evt.node) }
-  EventBus.updateNodeEvents subscribe { evt => StoryController.updateNode(evt.node) }
-  EventBus.destroyNodeEvents subscribe { evt => StoryController.deleteNode(evt.node) }
+  EventBus.createNodeEvents subscribe { evt => StoryController addNode evt.node }
+  EventBus.updateNodeEvents subscribe { evt => StoryController updateNode evt.node }
+  EventBus.destroyNodeEvents subscribe { evt => StoryController deleteNode evt.node }
 }
