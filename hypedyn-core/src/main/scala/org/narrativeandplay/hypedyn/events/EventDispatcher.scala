d26e777cc4daa1a05902ac46c70a63c171f794e9
Benedict Lee <benedictleejh@gmail.com>
2015-04-28 21:41:02 +0800

    Wire up the EventDispatcher to handle the clipboard related events

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/events/EventDispatcher.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/events/EventDispatcher.scala
@@ -1,5 +1,6 @@
 package org.narrativeandplay.hypedyn.events
 
+import org.narrativeandplay.hypedyn.clipboard.ClipboardController
 import org.narrativeandplay.hypedyn.story.StoryController
 
 object EventDispatcher {
@@ -10,4 +11,8 @@ object EventDispatcher {
   EventBus.createNodeEvents subscribe { evt => StoryController createNode evt.node }
   EventBus.updateNodeEvents subscribe { evt => StoryController updateNode (evt.uneditedNode, evt.editedNode) }
   EventBus.destroyNodeEvents subscribe { evt => StoryController destroyNode evt.node }
+
+  EventBus.cutNodeRequests subscribe { evt => StoryController find evt.nodeId foreach ClipboardController.cut }
+  EventBus.copyNodeRequests subscribe { evt => StoryController find evt.nodeId foreach ClipboardController.copy }
+  EventBus.pasteNodeRequests subscribe { evt => ClipboardController.paste() }
 }
