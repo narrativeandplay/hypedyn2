55d4ff19d7de1542c09715f2a5123122bc884303
Benedict Lee <benedictleejh@gmail.com>
2015-04-28 21:38:55 +0800

    Add toString to Node class to improve debugging information

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/internal/NodeImpl.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/internal/NodeImpl.scala
@@ -2,4 +2,6 @@ package org.narrativeandplay.hypedyn.story.internal
 
 import org.narrativeandplay.hypedyn.story.Node
 
-class NodeImpl(var name: String, var content: String, val id: Long) extends Node
+class NodeImpl(var name: String, var content: String, val id: Long) extends Node {
+  override def toString = s"Node { id: $id }"
+}
