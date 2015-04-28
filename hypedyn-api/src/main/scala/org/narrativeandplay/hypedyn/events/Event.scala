ff745c0f5164e357d6c8d86a04a73059d9438a41
Benedict Lee <benedictleejh@gmail.com>
2015-04-28 21:25:11 +0800

    Add clipboard related events for nodes

--- hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/events/Event.scala
+++ hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/events/Event.scala
@@ -6,29 +6,50 @@ import org.narrativeandplay.hypedyn.story.{Story, Node}
 sealed trait Event
 
 sealed trait Request extends Event
+
 case object NewNodeRequest extends Request
+
 sealed case class EditNodeRequest(nodeId: Long) extends Request
+
 sealed case class DeleteNodeRequest(nodeId: Long) extends Request
 
+sealed case class CutNodeRequest(nodeId: Long) extends Request
+
+sealed case class CopyNodeRequest(nodeId: Long) extends Request
+
+case object PasteNodeRequest extends Request
+
 sealed trait UIAction extends Event
+
 case object NewNode extends UIAction
+
 sealed case class EditNode(node: Node) extends UIAction
+
 sealed case class DeleteNode(node: Node) extends UIAction
 
 sealed trait Action extends Event
+
 sealed case class CreateNode(node: Node) extends Action
+
 sealed case class UpdateNode(uneditedNode: Node, editedNode: Node) extends Action
+
 sealed case class DestroyNode(node: Node) extends Action
 
 sealed trait Completion extends Event
+
 sealed case class NodeCreated(node: Node) extends Completion
+
 sealed case class NodeUpdated(node: Node) extends Completion
+
 sealed case class NodeDestroyed(node: Node) extends Completion
+
 sealed case class NodeSelected(nodeId: Long) extends Completion
+
 sealed case class NodeDeselected(nodeId: Long) extends Completion
 
 
 case object SaveEvent extends Event
+
 sealed case class LoadEvent(data: SaveHash) extends Event
 
 sealed case class StoryLoadedEvent(story: Story) extends Event
--- hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/events/EventBus.scala
+++ hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/events/EventBus.scala
@@ -14,41 +14,61 @@ object EventBus {
    * to breaking at any time
    */
   def requests = eventBus collect { case e: Request => e }
+
   def uiActions = eventBus collect { case e: UIAction => e }
+
   def actions = eventBus collect { case e: Action => e }
+
   def completions = eventBus collect { case e: Completion => e }
 
   /**
    * Request streams
    */
   def newNodeRequests = eventBus collect { case NewNodeRequest => NewNodeRequest }
+
   def editNodeRequests = eventBus collect { case e: EditNodeRequest => e }
+
   def deleteNodeRequests = eventBus collect { case e: DeleteNodeRequest => e }
+
+  def cutNodeRequests = eventBus collect { case e: CopyNodeRequest => e }
+
+  def copyNodeRequests = eventBus collect { case e: CopyNodeRequest => e }
+
+  def pasteNodeRequests = eventBus collect { case PasteNodeRequest => PasteNodeRequest }
 
   /**
    * UIAction streams
    */
   def newNodeEvents = eventBus collect { case NewNode => NewNode }
+
   def editNodeEvents = eventBus collect { case e: EditNode => e }
+
   def deleteNodeEvents = eventBus collect { case e: DeleteNode => e }
 
   /**
    * Action streams
    */
   def createNodeEvents = eventBus collect { case e: CreateNode => e }
+
   def updateNodeEvents = eventBus collect { case e: UpdateNode => e }
+
   def destroyNodeEvents = eventBus collect { case e: DestroyNode => e }
 
   /**
    * Completion streams
    */
   def nodeCreatedEvents = eventBus collect { case e: NodeCreated => e }
+
   def nodeUpdatedEvents = eventBus collect { case e: NodeUpdated => e }
+
   def nodeDestroyedEvents = eventBus collect { case e: NodeDestroyed => e }
+
   def nodeSelectedEvents = eventBus collect { case e: NodeSelected => e }
+
   def nodeDeselectedEvents = eventBus collect { case e: NodeDeselected => e }
 
 
   def saveEvents = eventBus.collect { case SaveEvent => SaveEvent }
+
   def loadEvents = eventBus.collect { case e: LoadEvent => e }
 }
