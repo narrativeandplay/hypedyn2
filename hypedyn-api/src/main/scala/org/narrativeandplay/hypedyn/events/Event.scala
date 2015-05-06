e5951f6404b7c256cd726b1f7eb9a7a0e5a65ed1
Benedict Lee <benedictleejh@gmail.com>
2015-05-06 19:45:33 +0800

    Add save and load events

--- hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/events/Event.scala
+++ hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/events/Event.scala
@@ -1,22 +1,19 @@
 package org.narrativeandplay.hypedyn.events
 
+import java.io.File
+
 import org.narrativeandplay.hypedyn.serialisation.SaveHash
 import org.narrativeandplay.hypedyn.story.{StoryLike, NodeLike}
 
 sealed trait Event
 
+
 sealed trait Request extends Event
-
 case object NewNodeRequest extends Request
-
 sealed case class EditNodeRequest(nodeId: Long) extends Request
-
 sealed case class DeleteNodeRequest(nodeId: Long) extends Request
-
 sealed case class CutNodeRequest(nodeId: Long) extends Request
-
 sealed case class CopyNodeRequest(nodeId: Long) extends Request
-
 case object PasteNodeRequest extends Request
 
 sealed trait UIAction extends Event
@@ -28,28 +25,19 @@ sealed case class EditNode(node: NodeLike) extends UIAction
 sealed case class DeleteNode(node: NodeLike) extends UIAction
 
 sealed trait Action extends Event
-
 sealed case class CreateNode(node: NodeLike) extends Action
-
 sealed case class UpdateNode(uneditedNode: NodeLike, editedNode: NodeLike) extends Action
-
 sealed case class DestroyNode(node: NodeLike) extends Action
 
+
 sealed trait Completion extends Event
-
 sealed case class NodeCreated(node: NodeLike) extends Completion
-
 sealed case class NodeUpdated(node: NodeLike) extends Completion
-
 sealed case class NodeDestroyed(node: NodeLike) extends Completion
-
 sealed case class NodeSelected(nodeId: Long) extends Completion
-
 sealed case class NodeDeselected(nodeId: Long) extends Completion
+sealed case class StoryLoadedEvent(story: StoryLike) extends Completion
 
 
-case object SaveEvent extends Event
-
-sealed case class LoadEvent(data: SaveHash) extends Event
-
-sealed case class StoryLoadedEvent(story: StoryLike) extends Event
+case class SaveEvent(saveFile: File) extends Event
+case class LoadEvent(saveFile: File) extends Event
