32c7135f56cda27762d29b5ff341a3e3be3c55aa
Benedict Lee <benedictleejh@gmail.com>
2015-05-06 19:48:53 +0800

    Expand plugin trait to handle what happens when a story is loaded
    The default is to do nothing, so it is optional for a plugin to override

--- hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/plugins/Plugin.scala
+++ hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/plugins/Plugin.scala
@@ -1,5 +1,8 @@
 package org.narrativeandplay.hypedyn.plugins
 
+import org.narrativeandplay.hypedyn.events.EventBus
+import org.narrativeandplay.hypedyn.story.StoryLike
+
 trait Plugin {
   /**
    * Returns the name of the plugin
@@ -10,4 +13,7 @@ trait Plugin {
    * Returns the version of the plugin (as per Semantic Versioning 2.0.0 - see http://semver.org/spec/v2.0.0.html)
    */
   def version: String //TODO: change this to a SemVer Version structure
+
+  EventBus.storyLoadedEvents subscribe { evt => onStoryLoaded(evt.story) }
+  def onStoryLoaded(story: StoryLike): Unit = {}
 }
