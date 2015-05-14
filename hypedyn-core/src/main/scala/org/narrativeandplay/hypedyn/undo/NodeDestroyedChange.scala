79253f4cc2bacc2fea61a0deea18db0c384435c8
Benedict Lee <benedictleejh@gmail.com>
2015-05-14 23:08:03 +0800

    Centralise the event sending by changes

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/undo/NodeDestroyedChange.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/undo/NodeDestroyedChange.scala
@@ -1,16 +1,16 @@
 package org.narrativeandplay.hypedyn.undo
 
-import org.narrativeandplay.hypedyn.events.{CreateNode, DestroyNode, EventBus}
+import org.narrativeandplay.hypedyn.events.UndoEventDispatcher
 import org.narrativeandplay.hypedyn.story.internal.Node
 
 class NodeDestroyedChange(destroyedNode: Node) extends Undoable {
   /**
    * Defines what to do when an undo action happens
    */
-  override def undo(): Unit = EventBus.send(CreateNode(destroyedNode, UndoEventSourceIdentity))
+  override def undo(): Unit = UndoEventDispatcher.createNode(destroyedNode)
 
   /**
    * Defines how to reverse an undo action
    */
-  override def redo(): Unit = EventBus.send(DestroyNode(destroyedNode, UndoEventSourceIdentity))
+  override def redo(): Unit = UndoEventDispatcher.destroyNode(destroyedNode)
 }
