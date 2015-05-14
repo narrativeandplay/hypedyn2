0ef9439b413f2b9fdb8f83e901c67a35203fff5d
Benedict Lee <benedictleejh@gmail.com>
2015-05-14 13:50:55 +0800

    Add event dispatcher for the core
    The event dispatcher is supposed to be the only object that deals with
    the events system. It calls various components, and uses their results
    to get the right things to happen.
    
    This centralises the event sending and receiving, making it easier to
    follow event flow.

---
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/events/CoreEventDispatcher.scala
@@ -0,0 +1,98 @@
+package org.narrativeandplay.hypedyn.events
+
+import org.narrativeandplay.hypedyn.plugins.PluginsController
+import org.narrativeandplay.hypedyn.serialisation.{IoController, Serialiser, AstMap, AstElement}
+import org.narrativeandplay.hypedyn.story.internal.Story
+import org.narrativeandplay.hypedyn.undo._
+import org.narrativeandplay.hypedyn.story.StoryController
+
+object CoreEventDispatcher {
+  val CoreEventSourceIdentity = "Core"
+
+  EventBus.NewNodeRequests foreach { evt => EventBus.send(NewNodeResponse(CoreEventSourceIdentity)) }
+  EventBus.EditNodeRequests foreach { evt =>
+    StoryController find evt.id foreach { n => EventBus.send(EditNodeResponse(n, CoreEventSourceIdentity)) }
+  }
+  EventBus.DeleteNodeRequests foreach { evt =>
+    StoryController find evt.id foreach { n => EventBus.send(DeleteNodeResponse(n, CoreEventSourceIdentity)) }
+  }
+
+  EventBus.CreateNodeEvents foreach { evt =>
+    val created = StoryController.create(evt.node)
+
+    if (evt.src != UndoEventSourceIdentity) {
+      UndoController.send(new NodeCreatedChange(created))
+    }
+
+    EventBus.send(NodeCreated(created, CoreEventSourceIdentity))
+  }
+  EventBus.UpdateNodeEvents foreach { evt =>
+    val updatedUnupdatedPair = StoryController.update(evt.node, evt.updatedNode)
+
+    updatedUnupdatedPair foreach { case (unupdated, updated) =>
+      if (evt.src != UndoEventSourceIdentity) {
+        UndoController.send(new NodeUpdatedChange(unupdated, updated))
+      }
+
+      EventBus.send(NodeUpdated(unupdated, updated, CoreEventSourceIdentity))
+    }
+  }
+  EventBus.DestroyNodeEvents foreach { evt =>
+    val destroyed = StoryController.destroy(evt.node)
+
+    destroyed foreach { n =>
+      if (evt.src != UndoEventSourceIdentity) {
+        UndoController.send(new NodeDestroyedChange(n))
+      }
+
+      EventBus.send(NodeDestroyed(n, CoreEventSourceIdentity))
+    }
+  }
+
+  EventBus.CutNodeRequests foreach { evt =>
+    StoryController find evt.id foreach { n => EventBus.send(CutNodeResponse(n, CoreEventSourceIdentity)) }
+  }
+  EventBus.CopyNodeRequests foreach { evt =>
+    StoryController find evt.id foreach { n => EventBus.send(CopyNodeResponse(n, CoreEventSourceIdentity)) }
+  }
+  EventBus.PasteNodeRequests foreach { evt => EventBus.send(PasteNodeResponse(CoreEventSourceIdentity)) }
+
+  EventBus.SaveRequests foreach { _ => EventBus.send(SaveResponse(CoreEventSourceIdentity)) }
+  EventBus.LoadRequests foreach { _ => EventBus.send(LoadResponse(CoreEventSourceIdentity)) }
+
+  EventBus.SaveDataEvents tumbling PluginsController.Plugins.size zip EventBus.SaveToFileEvents foreach {
+    case (pluginData, saveFileEvt) =>
+      pluginData.foldLeft(Map.empty[String, AstElement])({ case (m, SaveData(pluginName, data, _)) =>
+        m + (pluginName -> data)
+      }) map { pluginSaveDataMap =>
+        AstMap(pluginSaveDataMap.toSeq: _*)
+      } foreach { pluginSaveDataAstMap =>
+        val storyData = Serialiser serialise StoryController.story
+        val saveData = AstMap("story" -> storyData, "plugins" -> pluginSaveDataAstMap)
+
+        IoController.save(Serialiser toString saveData, saveFileEvt.file)
+
+        EventBus.send(StorySaved(CoreEventSourceIdentity))
+      }
+  }
+
+  EventBus.LoadFromFileEvents foreach { evt =>
+    val dataToLoad = IoController load evt.file
+    val dataAst = (Serialiser fromString dataToLoad).asInstanceOf[AstMap]
+
+    val pluginData = dataAst("plugins").asInstanceOf[AstMap].toMap
+
+    val story = Serialiser.deserialise[Story](dataAst("story"))
+    StoryController load story
+
+    EventBus.send(StoryLoaded(StoryController.story, CoreEventSourceIdentity))
+    EventBus.send(DataLoaded(pluginData, CoreEventSourceIdentity))
+  }
+
+  EventBus.NewStoryRequests foreach { _ => EventBus.send(NewStoryResponse(CoreEventSourceIdentity)) }
+  EventBus.CreateStoryEvents foreach { evt =>
+    StoryController.newStory(evt.title, evt.author, evt.desc)
+
+    EventBus.send(StoryLoaded(StoryController.story, CoreEventSourceIdentity))
+  }
+}
