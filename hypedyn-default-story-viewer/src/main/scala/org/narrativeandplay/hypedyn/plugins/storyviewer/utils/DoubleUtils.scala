4dd21e7c3c9e9f4ca185bb1fc4bc3c8586fa75b5
Benedict Lee <benedictleejh@gmail.com>
2015-05-14 19:35:23 +0800

    Add utilities for story viewer

---
+++ hypedyn-default-story-viewer/src/main/scala/org/narrativeandplay/hypedyn/plugins/storyviewer/utils/DoubleUtils.scala
@@ -0,0 +1,20 @@
+package org.narrativeandplay.hypedyn.plugins.storyviewer.utils
+
+object DoubleUtils {
+  private final val THRESHOLD = 1e-6
+
+  implicit class DoubleWithAlmostEquals(val d: Double) extends AnyVal {
+    def ~= (d2: Double) = (d - d2).abs < THRESHOLD
+
+    def !~= (d2: Double) = !(d ~= d2)
+
+    def <~ (d2: Double) = d - d2 < THRESHOLD
+
+    def <~= (d2: Double) = (d ~= d2) || (d <~ d2)
+
+    def >~ (d2: Double) = d - d2 > THRESHOLD
+
+    def >~= (d2: Double) = (d ~= d2) || (d >~ d2)
+  }
+
+}
