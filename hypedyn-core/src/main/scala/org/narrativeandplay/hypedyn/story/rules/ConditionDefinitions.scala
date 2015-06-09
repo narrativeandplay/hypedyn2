67fe652cc366d84f04431514219517944aed44e6
Benedict Lee <benedictleejh@gmail.com>
2015-06-09 14:59:31 +0800

    Add descriptions to condition definitions

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/rules/ConditionDefinitions.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/rules/ConditionDefinitions.scala
@@ -4,28 +4,28 @@ object ConditionDefinitions {
   def apply() = definitions
 
   private val definitions = List(
-    ConditionDefinition("NodeCondition", List(RuleParameter("node", Nodes),
-                                              RuleParameter("status",
-                                                            ListOfValues("visited",
-                                                                         "not visited",
-                                                                         "is previous",
-                                                                         "is not previous",
-                                                                         "current")))),
-    ConditionDefinition("LinkCondition", List(RuleParameter("link", Links),
-                                              RuleParameter("status", ListOfValues("followed", "not followed")))),
-    ConditionDefinition("BooleanFactValue", List(RuleParameter("fact", BooleanFacts),
-                                                 RuleParameter("state", ListOfValues("true", "false")))),
-    ConditionDefinition("IntegerFactComparison", List(RuleParameter("fact", IntegerFacts),
-                                                      RuleParameter("operator", ListOfValues("<",
-                                                                                             ">",
-                                                                                             ">=",
-                                                                                             "<=",
-                                                                                             "==",
-                                                                                             "!=")),
-                                                      RuleParameter("comparisonValue",
-                                                                    Union(List(
-                                                                      RuleParameter("input", UserInput),
-                                                                      RuleParameter("otherFact", IntegerFacts)))))
+    ConditionDefinition("NodeCondition", "Node", List(RuleParameter("node", Nodes),
+                                                      RuleParameter("status",
+                                                                    ListOfValues("visited",
+                                                                                 "not visited",
+                                                                                 "is previous",
+                                                                                 "is not previous",
+                                                                                 "current")))),
+    ConditionDefinition("LinkCondition", "Link", List(RuleParameter("link", Links),
+                                                      RuleParameter("status", ListOfValues("followed", "not followed")))),
+    ConditionDefinition("BooleanFactValue", "True/false fact", List(RuleParameter("fact", BooleanFacts),
+                                                                    RuleParameter("state", ListOfValues("true", "false")))),
+    ConditionDefinition("IntegerFactComparison", "Number fact", List(RuleParameter("fact", IntegerFacts),
+                                                                     RuleParameter("operator", ListOfValues("<",
+                                                                                                            ">",
+                                                                                                            ">=",
+                                                                                                            "<=",
+                                                                                                            "==",
+                                                                                                            "!=")),
+                                                                     RuleParameter("comparisonValue",
+                                                                                   Union(List(
+                                                                                     RuleParameter("input", UserInput),
+                                                                                     RuleParameter("otherFact", IntegerFacts)))))
     )
   )
 }
