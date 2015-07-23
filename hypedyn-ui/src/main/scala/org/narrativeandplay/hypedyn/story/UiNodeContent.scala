0452e27e9d10a86f79db3067259f04f207b4e1ca
Benedict Lee <benedictleejh@gmail.com>
2015-07-23 15:56:11 +0800

    Add Ruleset implementation to the UI

--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/story/UiNodeContent.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/story/UiNodeContent.scala
@@ -1,28 +1,50 @@
 package org.narrativeandplay.hypedyn.story
 
-import javafx.beans.property.SimpleMapProperty
-
 import scalafx.Includes._
-import scalafx.beans.property.StringProperty
-import scalafx.collections.ObservableMap
+import scalafx.beans.property.{ObjectProperty, StringProperty}
+import scalafx.collections.{ObservableBuffer, ObservableMap}
 
-import org.narrativeandplay.hypedyn.story.NodalContent.RulesetIndexes
+import org.narrativeandplay.hypedyn.story.NodalContent.{RulesetLike, RulesetIndexes}
 import org.narrativeandplay.hypedyn.story.rules.RuleLike
 
-class UiNodeContent(initText: String, initRulesets: Map[NodalContent.RulesetIndexes, UiRule]) extends NodalContent {
+class UiNodeContent(initText: String, initRulesets: List[UiNodeContent.UiRuleset]) extends NodalContent {
   val textProperty = StringProperty(initText)
-  val rulesetsProperty = ObservableMap(initRulesets.toSeq: _*)
-
-  initRulesets foreach { case (k, v) =>
-    rulesetsProperty.put(k, v)
-  }
+  val rulesetsProperty = ObservableBuffer(initRulesets)
 
   override def text: String = textProperty()
 
-  override def rulesets: Map[RulesetIndexes, RuleLike] = rulesetsProperty.toMap
+  override def rulesets: List[RulesetLike] = rulesetsProperty.toList
+
+  override def toString: String = s"${getClass.getCanonicalName}(text = '$text', rulesets = $rulesets"
 }
 
 object UiNodeContent {
-  def apply(initText: String, initRulesets: Map[NodalContent.RulesetIndexes, UiRule]) =
+  def apply(initText: String, initRulesets: List[UiRuleset]) =
     new UiNodeContent(initText, initRulesets)
+
+  class UiRuleset(val id: NodalContent.RulesetId,
+                  initName: String,
+                  initIndexes: RulesetIndexes,
+                  initRules: List[UiRule]) extends NodalContent.RulesetLike {
+    val nameProperty = StringProperty(initName)
+    val indexesProperty = ObjectProperty(initIndexes)
+    val rulesProperty = ObservableBuffer(initRules)
+
+    override def name: String = nameProperty()
+
+    override def rules: List[RuleLike] = rulesProperty.toList
+
+    override def indexes: RulesetIndexes = indexesProperty()
+
+    override def hashCode(): Int = id.hashCode()
+
+    override def equals(that: Any): Boolean = that match {
+      case that: UiRuleset => (that canEqual this) && (id == that.id)
+      case _ => false
+    }
+
+    def canEqual(that: Any): Boolean = that.isInstanceOf[UiRuleset]
+
+    override def toString: String = s"${getClass.getCanonicalName}(id = $id, name = $name, indexes = (${indexes.startIndex.index}, ${indexes.endIndex.index}) rules = $rules)"
+  }
 }
