171455a2eb6a2000a4c14b49aabe986652b83b01
Benedict Lee <benedictleejh@gmail.com>
2015-08-04 18:07:17 +0800

    Add a list of changed nodes to the node destroyed change
    This allows the undo of the removed actions and conditions that take
    place when a node is destroyed

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/undo/NodeDestroyedChange.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/undo/NodeDestroyedChange.scala
@@ -3,14 +3,24 @@ package org.narrativeandplay.hypedyn.undo
 import org.narrativeandplay.hypedyn.events.UndoEventDispatcher
 import org.narrativeandplay.hypedyn.story.internal.Node
 
-class NodeDestroyedChange(destroyedNode: Node) extends Undoable {
+class NodeDestroyedChange(destroyedNode: Node, changedNodes: Map[Node, Node]) extends Undoable {
   /**
    * Defines what to do when an undo action happens
    */
-  override def undo(): Unit = UndoEventDispatcher.createNode(destroyedNode)
+  override def undo(): Unit = {
+    UndoEventDispatcher.createNode(destroyedNode)
+    changedNodes foreach { case (unedited, edited) =>
+      UndoEventDispatcher.updateNode(edited, unedited)
+    }
+  }
 
   /**
    * Defines how to reverse an undo action
    */
-  override def redo(): Unit = UndoEventDispatcher.destroyNode(destroyedNode)
+  override def redo(): Unit = {
+    UndoEventDispatcher.destroyNode(destroyedNode)
+    changedNodes foreach { case (unedited, edited) =>
+      UndoEventDispatcher.updateNode(unedited, edited)
+    }
+  }
 }
