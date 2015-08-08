b053cdcb48b0035700c1381b010e311fc3b294ec
Benedict Lee <benedictleejh@gmail.com>
2015-08-08 13:07:11 +0800

    Add documentation for AST types

--- hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/serialisation/package.scala
+++ hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/serialisation/package.scala
@@ -1,5 +1,8 @@
 package org.narrativeandplay.hypedyn
 
 package object serialisation {
+  /**
+   * Define an AST field, i.e., a key-value pair, as a pair of a `String` and `AstElement`
+   */
   type AstField = (String, AstElement)
 }
