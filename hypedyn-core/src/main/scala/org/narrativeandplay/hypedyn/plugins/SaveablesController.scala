6ebe62dda65f777a67fc395141a057e3b8e85484
Benedict Lee <benedictleejh@gmail.com>
2015-08-12 17:39:02 +0800

    Add plugin controllers documentation

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/plugins/SaveablesController.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/plugins/SaveablesController.scala
@@ -1,6 +1,12 @@
 package org.narrativeandplay.hypedyn.plugins
 
+/**
+ * Controller for plugins implementing the Saveable interface
+ */
 object SaveablesController {
+  /**
+   * The list of plugins implementing Saveable
+   */
   val SaveablePlugins = PluginsController.plugins collect { case (name, plugin: Saveable) =>
     name -> plugin
   }
