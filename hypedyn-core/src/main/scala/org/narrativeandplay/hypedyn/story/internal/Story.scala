4e897e6c4c3a8bb8a6823ea46a6c50c20280a12f
Benedict Lee <benedictleejh@gmail.com>
2015-08-12 17:40:17 +0800

    Add data structures' documentation

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/internal/Story.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/internal/Story.scala
@@ -6,6 +6,17 @@ import org.narrativeandplay.hypedyn.story.Narrative.ReaderStyle.Standard
 import org.narrativeandplay.hypedyn.story.rules.internal.Rule
 import org.narrativeandplay.hypedyn.story.rules.Fact
 
+/**
+ * Class representing a story. The story class is immutable, and all operations on the story return a new copy
+ *
+ * @param title The title of the story
+ * @param author The author of the story
+ * @param description The description of the story
+ * @param metadata The metadata of the story
+ * @param nodes The story's nodes
+ * @param facts The story's facts
+ * @param rules The story-level rules
+ */
 case class Story(title: String = "Untitled",
                  author: String = "",
                  description: String = "",
@@ -13,30 +24,131 @@ case class Story(title: String = "Untitled",
                  nodes: List[Node] = Nil,
                  facts: List[Fact] = Nil,
                  rules: List[Rule] = Nil) extends Narrative {
+  /**
+   * Changes the title of the story
+   *
+   * @param newTitle The new title of the story
+   * @return A new story with the title changed
+   */
   def rename(newTitle: String) = new Story(newTitle, author, description, metadata, nodes, facts, rules)
+
+  /**
+   * Changes the author of the story
+   *
+   * @param newAuthor The new author of the story
+   * @return A new story with the author changed
+   */
   def changeAuthor(newAuthor: String) = new Story(title, newAuthor, description, metadata, nodes, facts, rules)
+
+  /**
+   * Changes the description of the story
+   *
+   * @param newDescription The new description of the story
+   * @return A new story with the description changed
+   */
   def changeDescription(newDescription: String) = new Story(title, author, newDescription, metadata, nodes, facts, rules)
+
+  /**
+   * Updates the metadata of the story
+   *
+   * @param newMetadata The new metadata of the story
+   * @return A new story with the metadata changed
+   */
   def updateMetadata(newMetadata: Story.Metadata) = new Story(title, author, description, newMetadata, nodes, facts, rules)
 
+  /**
+   * Adds a node to the story
+   *
+   * @param node The node to add to the story
+   * @return A new story with the specified node added
+   */
   def addNode(node: Node) = new Story(title, author, description, metadata, node :: nodes, facts, rules)
+
+  /**
+   * Updates a node of the story
+   *
+   * @param node The node to update
+   * @param newNode The updated version of the node
+   * @return A new story with the specified node updated
+   */
   def updateNode(node: Node, newNode: Node) = new Story(title, author, description, metadata,
                                                         newNode :: (nodes filter (_ != node)), facts, rules)
+
+  /**
+   * Removes a node from the story
+   * @param node The node to remove
+   * @return A new story with the specified node removed
+   */
   def removeNode(node: Node) = new Story(title, author, description, metadata, nodes filter (_ != node), facts, rules)
 
+  /**
+   * Adds a fact to the story
+   *
+   * @param fact The fact to add
+   * @return A new story with the specified fact added
+   */
   def addFact(fact: Fact) = new Story(title, author, description, metadata, nodes, fact :: facts, rules)
+
+  /**
+   * Updates a fact of the story
+   *
+   * @param fact The fact to update
+   * @param newFact The updated version of the fact
+   * @return A new story with the specified fact updated
+   */
   def updateFact(fact: Fact, newFact: Fact) = new Story(title, author, description, metadata, nodes,
                                                         newFact :: (facts filter (_ != fact)), rules)
+
+  /**
+   * Removes a fact from the story
+   *
+   * @param fact The fact to remove
+   * @return A new story with the specified fact removed
+   */
   def removeFact(fact: Fact) = new Story(title, author, description, metadata, nodes, facts filter (_ != fact), rules)
 
+  /**
+   * Adds a story-level rule
+   *
+   * @param rule The rule to add
+   * @return A new story with the specified fact added
+   */
   def addRule(rule: Rule) = new Story(title, author, description, metadata, nodes, facts, rule :: rules)
+
+  /**
+   * Updates a story-level rule
+   *
+   * @param rule The rule to update
+   * @param newRule The updated version of the rule
+   * @return A new story with the specified rule updated
+   */
   def updateRule(rule: Rule, newRule: Rule) = new Story(title, author, description, metadata, nodes, facts,
                                                         newRule :: (rules filter (_ != rule)))
+
+  /**
+   * Removes a story-level rule
+   *
+   * @param rule The rule to remove
+   * @return A new story with the specified rule removed
+   */
   def removeRule(rule: Rule) = new Story(title, author, description, metadata, nodes, facts, rules filter (_ != rule))
 
+  /**
+   * Returns all the rules in the story (text, node, and story rules)
+   */
   def allRules = rules ++ (nodes flatMap (_.rules)) ++ (nodes flatMap (_.content.rulesets) flatMap (_.rules))
 }
 
 object Story {
+
+  /**
+   * Class to represent story metadata
+   *
+   * @param comments The story's comments
+   * @param readerStyle The style of the reader
+   * @param isBackButtonDisabled Whether the back button is disabled
+   * @param isRestartButtonDisabled Whether the restart button is disabled
+   */
   case class Metadata(comments: String = "",
                       readerStyle: ReaderStyle = Standard,
                       isBackButtonDisabled: Boolean = false,
--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/rules/internal/Condition.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/story/rules/internal/Condition.scala
@@ -2,5 +2,11 @@ package org.narrativeandplay.hypedyn.story.rules.internal
 
 import org.narrativeandplay.hypedyn.story.rules.{RuleLike, Conditional}
 
+/**
+ * Class representing a condition instance
+ *
+ * @param conditionType The type of the instanced condition
+ * @param params The parameters and values of those parameters for the instanced condition
+ */
 case class Condition(conditionType: Conditional.ConditionType,
                      params: Map[RuleLike.ParamName, RuleLike.ParamValue]) extends Conditional
