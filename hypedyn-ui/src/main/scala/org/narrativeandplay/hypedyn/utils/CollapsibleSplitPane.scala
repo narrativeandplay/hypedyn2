45a3d42c2cc8f87367d35ad2682b02fbf6342851
Benedict Lee <benedictleejh@gmail.com>
2015-08-17 17:21:59 +0800

    Add documentation for utility classes

--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/utils/CollapsibleSplitPane.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/utils/CollapsibleSplitPane.scala
@@ -5,23 +5,48 @@ import scalafx.collections.ObservableBuffer
 import scalafx.scene.Node
 import scalafx.scene.control.SplitPane
 
+/**
+ * A SplitPane where items can be shown/hidden
+ */
 class CollapsibleSplitPane extends SplitPane {
   private val paneItems = ObservableBuffer.empty[Node]
 
+  /**
+   * Add an item to this pane
+   *
+   * @param node The item to add
+   */
   def add(node: Node): Unit = {
     paneItems += node
     items += node
   }
 
+  /**
+   * Remove an item from this pane
+   *
+   * @param node The item to remove
+   * @return The removed item
+   */
   def remove(node: Node): Node = {
     paneItems -= node
     items -= node
     node
   }
 
+  /**
+   * Hide an item in the pane
+   *
+   * @param node The item to hide
+   */
   def hide(node: Node): Unit = {
     items -= node
   }
+
+  /**
+   * Show an item in the pane
+   *
+   * @param node The item to show
+   */
   def show(node: Node): Unit = {
     if ((paneItems contains node) && isHidden(node)) {
       val shownItemIndexes = items.toList map { n => paneItems indexOf (n: Node) }
@@ -33,6 +58,19 @@ class CollapsibleSplitPane extends SplitPane {
     }
   }
 
+  /**
+   * Determines whether a given item is shown in the pane
+   *
+   * @param node The item to check
+   * @return True if the item is shown, false otherwise
+   */
   def isShown(node: Node): Boolean = items contains node
+
+  /**
+   * Determines whether a given item is hidden in the pane
+   *
+   * @param node The item to check
+   * @return True if the item is hidden, false otherwise
+   */
   def isHidden(node: Node): Boolean = !isShown(node)
 }
--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/utils/ScalaJavaImplicits.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/utils/ScalaJavaImplicits.scala
@@ -4,6 +4,9 @@ import java.util.function.{Function => JFunction, BiFunction}
 
 import scala.language.implicitConversions
 
+/**
+ * Implicit conversions from Scala function types to Java function types
+ */
 object ScalaJavaImplicits {
   implicit def function1ToFunction[T, U](f: T => U): JFunction[T, U] = new JFunction[T, U] {
     override def apply(t: T): U = f(t)
