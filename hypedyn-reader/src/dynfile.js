12642d878e2cbcbde5e29362d2363f0b325e4eee
Alex Mitchell <alexm@nus.edu.sg>
2015-10-09 11:30:06 +0800

    move creation of conditions and actions into functions

--- hypedyn-reader/src/dynfile.js
+++ hypedyn-reader/src/dynfile.js
@@ -1,9 +1,119 @@
+
+function createConditions(rulesetID, ruleID, conditions) {
+	for(var l=0; l < conditions.length; l++) {
+		var thisCondition = conditions[l];					// this condition
+		var conditionType = thisCondition.conditionType;	// type
+
+		// createCondition(func, func_args_arr, ruleID, not, id)
+		switch (conditionType) {
+			case "NodeCondition":
+				switch(thisCondition.params.status.value) {
+					case "visited":
+						createCondition(nodeVisited, [thisCondition.params.node.value], ruleID, false, l);
+						break;
+					case "not visited":
+						createCondition(nodeVisited, [thisCondition.params.node.value], ruleID, true, l);
+						break;
+					case "is previous":
+						createCondition(nodeIsPrevious, [thisCondition.params.node.value], ruleID, false, l);
+						break;
+					case "is not previous":
+						createCondition(nodeIsPrevious, [thisCondition.params.node.value], ruleID, true, l);
+						break;
+					case "current":
+						break;
+				}
+				break;
+			case "LinkCondition":
+				switch(thisCondition.params.status.value) {
+					case "followed":
+						createCondition(linkFollowed, [thisCondition.params.link.value], ruleID, false, l);
+						break;
+					case "not followed":
+						createCondition(linkFollowed, [thisCondition.params.link.value], ruleID, true, l);
+						break;
+				}
+				break;
+			case "BooleanFactValue":
+				switch(thisCondition.params.state.value) {
+					case "true":
+						createCondition(checkBoolFact, [thisCondition.params.fact.value], ruleID, false, l);
+						break;
+					case "false":
+						createCondition(checkBoolFact, [thisCondition.params.fact.value], ruleID, true, l);
+						break;
+				}
+				break;
+			case "IntegerFactComparison":
+				switch(thisCondition.params.comparisonValue) {
+					case "input":
+						// createCondition(compareNumFact, [19, '=', 'Input', 0], 27, false, 28);
+						// value is missing here, bug #23
+						createCondition(checkBoolFact, [thisCondition.params.fact,
+							thisCondition.params.operator, 'Input', 0], ruleID, false, l);
+						break;
+					case "otherFact":
+						//createCondition(compareNumFact, [19, '=', 'Fact', 19], 29, false, 30);
+						createCondition(compareNumFact, [thisCondition.params.fact,
+								thisCondition.params.operator, 'Fact', thisCondition.params.otherFact],
+							ruleID, false, l);
+						break;
+				}
+				break;
+		}
+	}
+}
+
+function createActions(ruleID, actions) {
+	for(var l=0; l < actions.length; l++) {
+		var thisAction = actions[l];
+		var actionType = thisAction.actionType;
+
+		// createAction(eventType, parentRuleID, func, args, id)
+		// eventType can be "enteredNode" "clickedLink" "anywhereCheck"
+		switch (actionType) {
+			case "LinkTo":
+				createAction("clickedLink", ruleID, gotoNode, [thisAction.params.node.value], l);
+				break;
+			case "ShowPopupNode":
+				createAction("clickedLink", ruleID, popup, [thisAction.params.node.value], l);
+				break;
+			case "UpdateText":
+				switch(thisAction.params.text.value) {
+					case "textInput":
+						//createAction("enteredNode", 63, replaceText, [62, "alternative text", "\"I'm not supposed to talk to strangers,\""], 98);
+						createAction("enteredNode", ruleID, replaceText, [rulesetID, "alternative text", thisAction.params.textInput.value], l);
+						break;
+					case "stringFactValue":
+						//createAction("enteredNode", 63, replaceText, [62, "text fact", 99], 101);
+						createAction("enteredNode", ruleID, replaceText, [rulesetID, "text fact", thisAction.params.stringFactValue.value], l);
+						break;
+				}
+				break;
+			case "UpdateBooleanFact":
+				//createAction("clickedLink", 4, setFact, [2, true], 5);
+				createAction("clickedLink", ruleID, setFact, [thisAction.params.fact.value, thisAction.params.value.value], l);
+				break;
+			case "UpdateStringFact":
+				//createAction("clickedLink", 8, setFact, [6, "[the fact text...........]"], 9);
+				createAction("clickedLink", ruleID, setFact, [thisAction.params.fact.value, thisAction.params.value.value], l);
+				break;
+			case "EnableAnywhereLinkToHere":
+				break;
+			case "ShowDisabledAnywhereLink":
+				break;
+			case "UpdateIntegerFacts":
+				break;
+		}
+	}
+}
+
 function loadStory() {
 	// try to avoid not-well-formed error (not working)
 	jQuery.ajaxSetup({ scriptCharset: "utf-8" , contentType: "application/json; charset=utf-8"});
 
 	// get the JSON file and parse it
-	jQuery.getJSON( "popup.dyn", function( data ) {
+	jQuery.getJSON( "LRRH-new.dyn", function( data ) {
 		var story = data.story;
 		var author = story.author; // unused
 		var description = story.description; // unused
@@ -56,114 +166,15 @@ function loadStory() {
 
 								// create conditions
 								if(conditions!=null) {
-									for(var l=0; l < conditions.length; l++) {
-										var thisCondition = conditions[l];					// this condition
-										var conditionType = thisCondition.conditionType;	// type
-
-										// createCondition(func, func_args_arr, ruleID, not, id)
-										switch (conditionType) {
-											case "NodeCondition":
-												switch(thisCondition.params.status.value) {
-													case "visited":
-														createCondition(nodeVisited, [thisCondition.params.node.value], ruleID, false, l);
-														break;
-													case "not visited":
-														createCondition(nodeVisited, [thisCondition.params.node.value], ruleID, true, l);
-														break;
-													case "is previous":
-														createCondition(nodeIsPrevious, [thisCondition.params.node.value], ruleID, false, l);
-														break;
-													case "is not previous":
-														createCondition(nodeIsPrevious, [thisCondition.params.node.value], ruleID, true, l);
-														break;
-													case "current":
-														break;
-												}
-												break;
-											case "LinkCondition":
-												switch(thisCondition.params.status.value) {
-													case "followed":
-														createCondition(linkFollowed, [thisCondition.params.link.value], ruleID, false, l);
-														break;
-													case "not followed":
-														createCondition(linkFollowed, [thisCondition.params.link.value], ruleID, true, l);
-														break;
-												}
-												break;
-											case "BooleanFactValue":
-												switch(thisCondition.params.state.value) {
-													case "true":
-														createCondition(checkBoolFact, [thisCondition.params.fact.value], ruleID, false, l);
-														break;
-													case "false":
-														createCondition(checkBoolFact, [thisCondition.params.fact.value], ruleID, true, l);
-														break;
-												}
-												break;
-											case "IntegerFactComparison":
-												switch(thisCondition.params.comparisonValue) {
-													case "input":
-														// createCondition(compareNumFact, [19, '=', 'Input', 0], 27, false, 28);
-														// value is missing here, bug #23
-														createCondition(checkBoolFact, [thisCondition.params.fact,
-															thisCondition.params.operator, 'Input', 0], ruleID, false, l);
-														break;
-													case "otherFact":
-														//createCondition(compareNumFact, [19, '=', 'Fact', 19], 29, false, 30);
-														createCondition(compareNumFact, [thisCondition.params.fact,
-															thisCondition.params.operator, 'Fact', thisCondition.params.otherFact],
-															ruleID, false, l);
-														break;
-												}
-												break;
-										}
-									}
+									createConditions(rulesetID, ruleID, conditions);
 								}
 
 								// create actions
 								if(actions!=null) {
-									for(var l=0; l < actions.length; l++) {
-										var thisAction = actions[l];
-										var actionType = thisAction.actionType;
-
-										// createAction(eventType, parentRuleID, func, args, id)
-										// eventType can be "enteredNode" "clickedLink" "anywhereCheck"
-										switch (actionType) {
-											case "LinkTo":
-												createAction("clickedLink", ruleID, gotoNode, [thisAction.params.node.value], l);
-												break;
-											case "ShowPopupNode":
-												createAction("clickedLink", ruleID, popup, [thisAction.params.node.value], l);
-												break;
-											case "UpdateText":
-												switch(thisAction.params.text.value) {
-													case "textInput":
-														//createAction("enteredNode", 63, replaceText, [62, "alternative text", "\"I'm not supposed to talk to strangers,\""], 98);
-														createAction("enteredNode", ruleID, replaceText, [rulesetID, "alternative text", thisAction.params.textInput.value], l);
-														break;
-													case "stringFactValue":
-														//createAction("enteredNode", 63, replaceText, [62, "text fact", 99], 101);
-														createAction("enteredNode", ruleID, replaceText, [rulesetID, "text fact", thisAction.params.stringFactValue.value], l);
-														break;
-												}
-												break;
-											case "UpdateBooleanFact":
-												break;
-											case "UpdateStringFact":
-												break;
-											case "EnableAnywhereLinkToHere":
-												break;
-											case "ShowDisabledAnywhereLink":
-												break;
-											case "UpdateIntegerFacts":
-												break;
-										}
-									}
+									createActions(ruleID, actions);
 								}
-
 							}
 						}
-
 					}
 				}
 
@@ -193,31 +204,4 @@ function loadStory() {
 		nonpageflip_init();
 	});
 
-	// set the start node
-	/*
-	setStartNode(1);
-	createNode("start", "This is the story of a girl named Little Red Riding Hood, which she was called because of the red hood that she often wore.\n\nOne day she was walking through the forest.\n", false, 1);
-	createLink(1, 94, 102, 20);
-		createRule(20, "link", "if", "and", true, 21);
-			createCondition(nodeVisited, [3], 21, false, 24);
-			createAction("clickedLink", 21, gotoNode, [16], 25);
-	createLink(1, 161, 167, 4);
-		createRule(4, "link", "if", "and", true, 7);
-			createAction("clickedLink", 7, gotoNode, [2], 17);
-
-	createNode("forest", "In the forest, Red came across a young man with a nasty smile.\n\n\"Where are you going, little girl?\" he asked. \n\n\"I'm off to see my sick granny,\" she said.\n\nWell, you can probably guess what happened next.", false, 2);
-	createLink(2, 199, 203, 10);
-		createRule(10, "link", "if", "and", true, 11);
-			createAction("clickedLink", 11, gotoNode, [3], 18);
-
-	createNode("end", "*** The End ***\n\nback to start", false, 3);
-	createLink(3, 17, 30, 13);
-		createRule(13, "link", "if", "and", true, 14);
-			createAction("clickedLink", 14, gotoNode, [1], 19);
-
-	createNode("Hood details", "Her hood was a magic garment, given to her by her grandmother. It could kill anyone who tried to harm the wearer.\n\nBut not immediately. And in a most painful manner.\n\nMeanwhile, in the forest...\n", false, 16);
-	createLink(16, 185, 191, 17);
-		createRule(17, "link", "if", "and", true, 18);
-			createAction("clickedLink", 18, gotoNode, [2], 20);
-			*/
 }
