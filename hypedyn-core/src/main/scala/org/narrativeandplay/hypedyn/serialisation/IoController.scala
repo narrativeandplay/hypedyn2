d35dce994296b3072d9c6e215ce9300ec1d7d650
Benedict Lee <benedictleejh@gmail.com>
2015-10-24 00:00:48 +0800

    Add function for copying files from a JAR

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/serialisation/IoController.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/serialisation/IoController.scala
@@ -1,8 +1,11 @@
 package org.narrativeandplay.hypedyn.serialisation
 
 import java.io.{PrintWriter, File}
+import java.net.JarURLConnection
 
 import scala.io.Source
+
+import org.apache.commons.io.FileUtils
 
 /**
  * Controller handling file read/write
@@ -34,5 +37,45 @@ object IoController {
     fileSource.close()
 
     data
+  }
+
+  /**
+   * Copies the contents of a resource folder from inside the classpath to the filesystem
+   *
+   * Adapted from http://stackoverflow.com/a/2993908
+   *
+   * @param resourceName The resource folder whose contents are to be copied; if a relative resource path is given,
+   *                     it will be relative to org.narrativeandplay.hypedyn.serialisation
+   * @param directory The directory where the files are to be copied to, if the folder does not exist, it will be
+   *                  created
+   */
+  def copyResourceToFilesystem(resourceName: String, directory: File): Unit = {
+    if (!directory.exists()) {
+      directory.mkdirs()
+    }
+
+    val jarConnection = getClass.getResource(resourceName).openConnection().asInstanceOf[JarURLConnection]
+    val jarFile = jarConnection.getJarFile
+
+    if (!jarConnection.getJarEntry.isDirectory)
+      throw new IllegalArgumentException("Resource to be copied must be a directory")
+
+    import scala.collection.JavaConversions._
+    jarFile.entries() foreach { entry =>
+      if (entry.getName startsWith jarConnection.getEntryName) {
+        val filename = entry.getName replace (jarConnection.getEntryName, "")
+
+        if (!entry.isDirectory) {
+          val in = jarFile.getInputStream(entry)
+          FileUtils.copyInputStreamToFile(in, new File(directory, filename))
+          in.close()
+        }
+        else {
+          if (!directory.exists()) {
+            new File(directory, filename).mkdir()
+          }
+        }
+      }
+    }
   }
 }
