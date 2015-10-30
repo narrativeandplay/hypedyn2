64ce64b5f5959c60c99da9779bddca95f0c38f89
Benedict Lee <benedictleejh@gmail.com>
2015-10-30 23:21:04 +0800

    Fix #38
    The problem was that asking the undo manager to forget history only
    forgets the undo history, and leaves redo history alone. So, to forget
    redo history, we need to replace the entire undo manager.
    
    The result is wrapping the undo manager in a property, and expose the
    streams of the properties we need via EasyBind flat mappings (for an
    effective auto-rebind when the undo manager changes).

--- hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/undo/UndoController.scala
+++ hypedyn-core/src/main/scala/org/narrativeandplay/hypedyn/undo/UndoController.scala
@@ -1,14 +1,60 @@
 package org.narrativeandplay.hypedyn.undo
 
-import javafx.beans.value.ObservableBooleanValue
+import java.lang
+import java.util.function.{Function => JFunction}
+import javafx.beans.value.ObservableValue
 
-import org.fxmisc.undo.UndoManagerFactory
+import scalafx.beans.property.ObjectProperty
 
+import org.fxmisc.easybind.EasyBind
+import org.fxmisc.undo.{UndoManager, UndoManagerFactory}
 /**
  * Controller handling undo events
  */
 object UndoController {
-  private val undoManager = UndoManagerFactory.unlimitedHistoryUndoManager(
+  private val undoManager = ObjectProperty(UndoManagerFactory.unlimitedHistoryUndoManager(
+    UndoableStream.changes,
+    { c: Undoable => c.redo() },
+    { c: Undoable => c.undo() },
+    { (c1: Undoable, c2: Undoable) => c1 mergeWith c2 }
+  ))
+
+  /**
+   * Observable stream of whether the current position within the undo manager's
+   * history is the same as the last marked position.
+   */
+  val atMarkedPosition = EasyBind monadic undoManager flatMap new JFunction[UndoManager, ObservableValue[lang.Boolean]] {
+    override def apply(t: UndoManager): ObservableValue[lang.Boolean] = t.atMarkedPositionProperty()
+  }
+
+  /**
+   * Observable stream of whether undo is available
+   */
+  val undoAvailable = EasyBind monadic undoManager flatMap new JFunction[UndoManager, ObservableValue[lang.Boolean]] {
+    override def apply(t: UndoManager): ObservableValue[lang.Boolean] = t.undoAvailableProperty()
+  }
+
+  /**
+   * Observable stream of whether redo is available
+   */
+  val redoAvailable = EasyBind monadic undoManager flatMap new JFunction[UndoManager, ObservableValue[lang.Boolean]] {
+    override def apply(t: UndoManager): ObservableValue[lang.Boolean] = t.redoAvailableProperty()
+  }
+
+  /**
+   * Undo a change
+   */
+  def undo(): Unit = undoManager().undo()
+
+  /**
+   * Redo a change
+   */
+  def redo(): Unit = undoManager().redo()
+
+  /**
+   * Clears the undo history
+   */
+  def clearHistory(): Unit = undoManager() = UndoManagerFactory.unlimitedHistoryUndoManager(
     UndoableStream.changes,
     { c: Undoable => c.redo() },
     { c: Undoable => c.undo() },
@@ -16,40 +62,7 @@ object UndoController {
   )
 
   /**
-   * Observable stream of whether the current position within the undo manager's
-   * history is the same as the last marked position.
-   */
-  val atMarkedPosition: ObservableBooleanValue = undoManager.atMarkedPositionProperty()
-
-  /**
-   * Undo a change
-   */
-  def undo(): Unit = undoManager.undo()
-
-  /**
-   * Redo a change
-   */
-  def redo(): Unit = undoManager.redo()
-
-  /**
-   * Clears the undo history
-   */
-  def clearHistory(): Unit = {
-    undoManager.forgetHistory()
-  }
-
-  /**
-   * Observable stream of whether undo is available
-   */
-  def undoAvailable: ObservableBooleanValue = undoManager.undoAvailableProperty()
-
-  /**
-   * Observable stream of whether redo is available
-   */
-  def redoAvailable: ObservableBooleanValue = undoManager.redoAvailableProperty()
-
-  /**
    * Marks the current position in the undo queue
    */
-  def markCurrentPosition(): Unit = undoManager.mark()
+  def markCurrentPosition(): Unit = undoManager().mark()
 }
