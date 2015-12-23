2c13e493950c0e9c9ad84de5217c24f8c5fb1f06
Benedict Lee <benedictleejh@gmail.com>
2015-12-23 18:02:26 +0800

    Add more functions to complex numbers

--- hypedyn-default-story-viewer/src/main/scala/org/narrativeandplay/hypedyn/storyviewer/utils/CubicPolynomial.scala
+++ hypedyn-default-story-viewer/src/main/scala/org/narrativeandplay/hypedyn/storyviewer/utils/CubicPolynomial.scala
@@ -1,7 +1,9 @@
 package org.narrativeandplay.hypedyn.storyviewer.utils
 
+import DoubleUtils._
+
 /**
- * Class representing a cubic polynomial
+ * Class representing a cubic polynomial of the form ax^3 + bx^2 + cx + d
  *
  * @param a The coefficient of the cube term. This cannot be 0.
  * @param b The coefficient of the square term.
@@ -10,16 +12,23 @@ package org.narrativeandplay.hypedyn.storyviewer.utils
  */
 case class CubicPolynomial(a: Double, b: Double, c: Double, d: Double) {
   assert(a != 0, "a must not be 0")
+  import math.pow
 
   def roots: List[Double] = {
-    import math.pow
-    val discriminant = 18 * a * b * c * d -
-      4 * pow(b, 3) * d +
-      pow(b, 2) * pow(c, 2) -
-      4 * a * pow(c, 3) -
-      27 * pow(a, 2) * pow(d, 2)
+    val A_2 = b / a
+    val A_1 = c / a
+    val A_0 = d / a
+
+    val x_inflec = -A_2 / 3
+
+    val D = A_2 * A_2 - 3 * A_1
+
+
     ???
   }
+
+  def apply = valueAt _
+  def valueAt(x: Double) = a * pow(x, 3) + b * pow(x, 2) + c * x + d
 }
 
 /**
@@ -28,14 +37,28 @@ case class CubicPolynomial(a: Double, b: Double, c: Double, d: Double) {
 sealed case class ComplexNumber(a: Double, b: Double) {
   def conjugate = ComplexNumber(a, -b)
   def unary_~ = conjugate
-  def abs = math.sqrt(a*a + b*b)
+  def absSqr = a*a + b*b
+  def abs = math.sqrt(absSqr)
 
   def +(z: ComplexNumber) = ComplexNumber(a + z.a, b + z.b)
   def -(z: ComplexNumber) = ComplexNumber(a - z.a, b - z.b)
 
   def *(z: ComplexNumber) = ComplexNumber(a * z.a - b * z.b, b * z.a + a * z.b)
+  def /(z: ComplexNumber) = ComplexNumber((a * z.a + b * z.b)/z.absSqr, (b * z.a - a * z.b)/z.absSqr)
+
+  def isReal = b ~= 0
+  def isComplex = !isReal
+
+  def toDouble = if (isReal) a else abs
+
+  override def toString = s"$a + ${b}i"
+
+  override def equals(obj: Any): Boolean = {
+    case z: ComplexNumber => (a ~= z.a) && (b ~= z.b)
+    case _ => false
+  }
 }
 
 object ComplexNumber {
-  def fromDouble(d: Double) = ComplexNumber(d, 0)
+  def apply(d: Double) = ComplexNumber(d, 0)
 }
