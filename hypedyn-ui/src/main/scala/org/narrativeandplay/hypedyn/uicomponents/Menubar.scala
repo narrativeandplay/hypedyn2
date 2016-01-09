286f31e0b6a2935c67f001dd22ec62006b4b45e0
Benedict Lee <benedictleejh@gmail.com>
2016-01-09 19:01:24 +0800

    Update comment to reflect current code

--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/uicomponents/Menubar.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/uicomponents/Menubar.scala
@@ -25,9 +25,7 @@ class Menubar(mainStageFocused: ReadOnlyBooleanProperty) extends MenuBar {
    */
   class MenuItem(name: String) extends scalafx.scene.control.MenuItem(name) {
     // Disable the menu items when the main window is not being selected,effectively preventing the shortcuts from
-    // propagating to node editor windows. The function `flatMap` is a Java method call to a generic method. Due to
-    // limitations in the Scala compiler, the type parameters of these method calls must be provided for Scala to
-    // correctly type the results.
+    // propagating to node editor windows.
     disable <== !mainStageFocused
   }
 
