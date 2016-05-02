102b06070ded4754e1da2c9a757dfc5b4c4da358
Alex Mitchell <alexm@nus.edu.sg>
2016-05-02 23:24:02 +0800

    update version numbers to 0.22.2-alpha

--- hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/Main.scala
+++ hypedyn-ui/src/main/scala/org/narrativeandplay/hypedyn/Main.scala
@@ -144,7 +144,7 @@ object Main extends JFXApp {
     graphic = new ImageView(icon)
     contentText =
       """Hypertext Fiction Editor
-        |Version 1.0
+        |Version 0.22.2-alpha
       """.stripMargin
   }
 
