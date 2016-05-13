9e9522cff5ba4b753dc1e2cb01acf3bdb413118a
Benedict Lee <benedictleejh@gmail.com>
2016-05-13 21:33:55 +0800

    Hide pretty printer from Logger interface
    This prevents internal pretty printing details from leaking out into the
    Logger interface

--- hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/logging/Logger.scala
+++ hypedyn-api/src/main/scala/org/narrativeandplay/hypedyn/logging/Logger.scala
@@ -6,40 +6,43 @@ import org.narrativeandplay.hypedyn.story.NodalContent.RulesetId
 import org.narrativeandplay.hypedyn.story.NodeId
 import org.narrativeandplay.hypedyn.story.rules.{FactId, RuleId}
 
-object Logger extends PrettyPrinter {
+object Logger {
   private val logger = grizzled.slf4j.Logger[this.type]
 
-  override val defaultIndent = 2
+  private val prettyPrinter = new PrettyPrinter {
+    override val defaultIndent = 2
 
-  override def any (a : Any) : Doc =
-    if (a == null)
-      "null"
-    else
-      a match {
-        case v : Vector[_] => list (v.toList, "Vector ", any)
-        case m : Map[_,_]  => list (m.toList, "Map ", any)
-        case Nil           => "Nil"
-        case l : List[_]   => list (l, "List ", any)
-        case (l, r)        => any (l) <+> "->" <+> any (r)
-        case Some(v)       => s"Some ($v)"
-        case None          => "None"
-        case NodeId(id)    => s"NodeId ($id)"
-        case FactId(id)    => s"FactId ($id)"
-        case RuleId(id)    => s"RuleId ($id)"
-        case RulesetId(id) => s"RulesetId ($id)"
-        case p : Product   =>
-          val fields = p.getClass.getDeclaredFields map (_.getName) zip p.productIterator.to
-          if (fields.length == 0) {
-            s"${p.productPrefix}"
-          } else {
-            list (fields.toList,
-                  s"${p.productPrefix} ",
-                  any)
-          }
-        case s : String    => dquotes (text (s))
-        case _             => a.toDoc
-      }
+    override def any (a : Any) : Doc =
+      if (a == null)
+        "null"
+      else
+        a match {
+          case v : Vector[_] => list (v.toList, "Vector ", any)
+          case m : Map[_,_]  => list (m.toList, "Map ", any)
+          case Nil           => "Nil"
+          case l : List[_]   => list (l, "List ", any)
+          case (l, r)        => any (l) <+> "->" <+> any (r)
+          case Some(v)       => s"Some ($v)"
+          case None          => "None"
+          case NodeId(id)    => s"NodeId ($id)"
+          case FactId(id)    => s"FactId ($id)"
+          case RuleId(id)    => s"RuleId ($id)"
+          case RulesetId(id) => s"RulesetId ($id)"
+          case p : Product   =>
+            val fields = p.getClass.getDeclaredFields map (_.getName) zip p.productIterator.to
+            if (fields.length == 0) {
+              s"${p.productPrefix}"
+            } else {
+              list (fields.toList,
+                    s"${p.productPrefix} ",
+                    any)
+            }
+          case s : String    => dquotes (text (s))
+          case _             => a.toDoc
+        }
+  }
 
+  import prettyPrinter.{pretty, any}
   def info(msg: => Any) = logger.info(pretty(any(msg)))
   def info(msg: => Any, t: => Throwable) = logger.info(pretty(any(msg)), t)
 
